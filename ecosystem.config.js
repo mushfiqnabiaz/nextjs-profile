d3dc8800d647630acb544d21a24e7f3ed463c63b
MD. Mushfiqur Rahaman <nabiaz@MDs-MacBook-Air.local>
2025-09-22 00:01:35 +0600

    ecofix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,20 +1,21 @@
 module.exports = {
-  apps: [{
-    name: 'portfolio',
-    script: 'npm',
-    args: 'start',
-    cwd: '/home/portfolio',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
-    env: {
-      NODE_ENV: 'production',
-      PORT: 3000
-    },
-    error_file: './logs/err.log',
-    out_file: './logs/out.log',
-    log_file: './logs/combined.log',
-    time: true
-  }]
+  apps: [
+    {
+      name: 'portfolio',
+      script: 'npm',
+      args: 'start',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000
+      },
+      error_file: 'logs/err.log',
+      out_file: 'logs/out.log',
+      log_file: 'logs/combined.log',
+      time: true
+    }
+  ]
 }
